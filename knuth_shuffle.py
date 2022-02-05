df1e4f21b1cf70d3a70b5cada7428c60620a0b62
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-05 22:50:14 +0200

    Added historical comment to Knuth shuffle.

--- knuth_shuffle.py
+++ knuth_shuffle.py
@@ -2,6 +2,11 @@ from random import randint
 
 
 def knuth_shuffle(sequence: list):
+    """
+    Proposition: [Fisher-Yates 1938].
+    Knuth shuffling algorithm.
+    Produces a uniformly random permutation of the input array in linear time.
+    """
     for i in range(len(sequence)):
         random_index = randint(0, i)
         sequence[i], sequence[random_index] = sequence[random_index], sequence[i]
