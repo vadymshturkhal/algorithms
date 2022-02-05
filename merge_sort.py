cacd5871d1454ce5c4dbdcccb43caceed83645f2
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-05 22:21:32 +0200

    Add comment to trick method

--- merge_sort.py
+++ merge_sort.py
@@ -12,7 +12,8 @@ def merge_sort(sequence: list, tmp: list = None, left_bound=None, right_bound=No
     merge_sort(tmp, sequence, left_bound, mid)
     merge_sort(tmp, sequence, mid, right_bound)
 
-    sequence, tmp = tmp, sequence
+    sequence, tmp = tmp, sequence  # What the heck?
+
     merge(sequence, tmp, left_bound, mid, right_bound)
 
 
