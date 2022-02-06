2730b6a3ca730874312205a149786bb73aee783e
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-06 23:38:55 +0200

    Realized break number algorithm which calculated quantity of ordered sums of terms. And not printed them.

--- break_number_to_ordered_sum_of_terms.py
+++ break_number_to_ordered_sum_of_terms.py
@@ -1,10 +1,24 @@
-def break_number_to_ordered_sum_of_terms(number: int):
-    """Assume number >= 0"""
-    terms = [_ for _ in range(number)]
+def break_number_to_ordered_sum_of_terms(number: int, terms=None):
+    """
+        Assume number >= 0.
+        Default terms from 1 to number - 1.
+    """
+    if number == 0:
+        return 1
+    if number < 0:
+        return 0
+
+    if terms is None:
+        terms = [_ for _ in range(1, number)]
+
+    current_sum = 0
+    for term in terms:
+        current_sum += break_number_to_ordered_sum_of_terms(number - term, terms)
+
+    return current_sum
 
 
 if __name__ == '__main__':
-    N = 5
-    break_number_to_ordered_sum_of_terms(N)
-
-
+    N = 3
+    quantity = break_number_to_ordered_sum_of_terms(N)
+    print(quantity)
