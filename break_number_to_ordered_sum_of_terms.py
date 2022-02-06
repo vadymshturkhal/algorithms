06b9c53054fb3388697009f2c8eef144126d9061
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-07 00:51:34 +0200

    Implemented memoization for break_number... algorithm. Now it's working much faster. And how can we test the answer for N == 100?

--- break_number_to_ordered_sum_of_terms.py
+++ break_number_to_ordered_sum_of_terms.py
@@ -1,8 +1,9 @@
 # FIXME use dynamic programming
-def break_number_to_ordered_sum_of_terms(number: int, terms=None):
+def break_number_to_ordered_sum_of_terms(number: int, terms=None, memoize=None):
     """
         Assume number >= 0.
         Default terms from 1 to number - 1.
+        Returns quantity of ordered sum of terms from "terms".
     """
     if number == 0:
         return 1
@@ -11,12 +12,20 @@ def break_number_to_ordered_sum_of_terms(number: int, terms=None):
 
     if terms is None:
         terms = [_ for _ in range(1, number)]
+
+    if memoize is None:
         memoize = {}
 
     current_sum = 0
     for term in terms:
         current_term = number - term
-        current_sum += break_number_to_ordered_sum_of_terms(current_term, terms)
+        if memoize.get(current_term) is None:
+            current_sums_quantity = break_number_to_ordered_sum_of_terms(current_term, terms, memoize)
+            memoize[current_term] = current_sums_quantity
+        else:
+            current_sums_quantity = memoize[current_term]
+
+        current_sum += current_sums_quantity
 
     return current_sum
 
@@ -26,4 +35,6 @@ if __name__ == '__main__':
     quantity = break_number_to_ordered_sum_of_terms(N)
     print(quantity)
 
-    a = {}
+    N = 100
+    quantity = break_number_to_ordered_sum_of_terms(N)
+    print(quantity)
