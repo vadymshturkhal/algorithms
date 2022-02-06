655bc0b7561baab9468d21801f8ab48a0539fc33
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-06 23:26:37 +0200

    Trying to implement formula for break number

---
+++ break_number_to_ordered_sum_of_terms.py
@@ -0,0 +1,9 @@
+def break_number_to_ordered_sum_of_terms(number: int):
+    terms = [_ for _ in range(number)]
+
+
+if __name__ == '__main__':
+    N = 5
+    break_number_to_ordered_sum_of_terms(N)
+
+
