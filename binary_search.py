5faceaf62f0418f9817c7ec98fb854b4be32716e
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-06 20:33:52 +0200

    Implemented binary search which returns index of element or -1

--- binary_search.py
+++ binary_search.py
@@ -1,11 +1,21 @@
-def binary_search(sequence: list) -> tuple:
+def binary_search(sequence: list, element) -> int:
     left_bound = 0
     right_bound = len(sequence) - 1
 
-    return left_bound, right_bound
+    while left_bound <= right_bound:
+        mid = (left_bound + right_bound) // 2
+        if element < sequence[mid]:
+            right_bound = mid - 1
+        elif element > sequence[mid]:
+            left_bound = mid + 1
+        else:
+            return mid
+
+    return -1
 
 
 if __name__ == '__main__':
-    a = [_ for _ in range(10)]
-    bounds = binary_search(a)
-    print(bounds)
+    a = [i + 1 for i in range(10)]
+    print(binary_search(a, 9))
+    print(binary_search(a, 10))
+    print(binary_search(a, 11))
