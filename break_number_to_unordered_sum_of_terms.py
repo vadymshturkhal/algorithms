7647e9120a2c8c5c04b941c0314da90dcb88699d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-07 17:24:12 +0200

    Added theorem to comment.

--- break_number_to_unordered_sum_of_terms.py
+++ break_number_to_unordered_sum_of_terms.py
@@ -1,8 +1,10 @@
 def break_number_to_unordered_sum_of_pairs(number: int):
     """
         Assume number >= 0.
-        Default terms from 1 to number - 1.
+        Default terms from 1 to number.
         Returns quantity of unordered sum of terms from "terms".
+        Theorem:
+            F(n: [n1, n2,...,nk]) = F(n - n1: [n1, n2,...,nk]) + F(n: [n2,...,nk])
     """
     return 0
 
