b841ee78eb49cc5047df5953cc16af0fd6dafc1b
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-07 17:31:38 +0200

    Added Hardy-Ramanujan theorem to comment.

--- break_number_to_unordered_sum_of_terms.py
+++ break_number_to_unordered_sum_of_terms.py
@@ -5,7 +5,12 @@ def break_number_to_unordered_sum_of_pairs(number: int):
         Returns quantity of unordered sum of terms from "terms".
         Theorem:
             F(n: [n1, n2,...,nk]) = F(n - n1: [n1, n2,...,nk]) + F(n: [n2,...,nk])
+        Theorem:
+            F(n: [n1, n2,...,nk]) = ?. Theorem doesn't exist.
+        Theorem Hardy–Ramanujan:
+            F(n: [n1, n2,...,nk]) ~ 1 / (4*n*sqrt(3)) * e**(pi*sqrt(2/3)*sqrt(n - 1/24)
     """
+
     return 0
 
 
