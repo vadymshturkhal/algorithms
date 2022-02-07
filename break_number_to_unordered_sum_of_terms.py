93342a7458acc9cb6a25f799beb1277dbd9a7c90
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-07 11:09:46 +0200

    Implemented signature.

---
+++ break_number_to_unordered_sum_of_terms.py
@@ -0,0 +1,13 @@
+def break_number_to_unordered_sum_of_pairs(number: int):
+    """
+        Assume number >= 0.
+        Default terms from 1 to number - 1.
+        Returns quantity of unordered sum of terms from "terms".
+    """
+    return 0
+
+
+if __name__ == '__main__':
+    N = 3
+    quantity = break_number_to_unordered_sum_of_pairs(N)
+    print(quantity)
