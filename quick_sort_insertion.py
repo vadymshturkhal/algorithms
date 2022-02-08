fc4b818b4738a78537eb01652d820c61ad39a69a
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-08 17:41:58 +0200

    Fixed critical bug with insertion sort. Now it works faster than quick sort. Used import instead of copy paste.

--- quick_sort_insertion.py
+++ quick_sort_insertion.py
@@ -1,19 +1,7 @@
 import random
+from insertion_sort import insertion_sort
 
 CUTOFF = 5
-
-
-def insertion_sort(sequence: list, left_bound=None, right_bound=None):
-    if left_bound is None:
-        left_bound = 0
-        right_bound = len(sequence)
-
-    for i in range(left_bound, right_bound):
-        for j in range(i, 0, -1):
-            if sequence[j - 1] > sequence[j]:
-                sequence[j], sequence[j - 1] = sequence[j - 1], sequence[j]
-            else:
-                break
 
 
 def quick_sort_insertion(sequence: list, low: int = None, high: int = None):
