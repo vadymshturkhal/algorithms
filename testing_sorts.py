071921f4a7bdc8b45ef26fda6c115407040790f1
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-08 17:20:27 +0200

    Added test for quick sort using insertion sort.

--- testing_sorts.py
+++ testing_sorts.py
@@ -2,6 +2,7 @@ import time
 from merge_sort import merge_sort
 from merge_sort_insertion import merge_sort_insertion
 from quick_sort import quick_sort
+from quick_sort_insertion import quick_sort_insertion
 
 N = 900
 
@@ -21,3 +22,9 @@ a = [_ for _ in range(N, -1, -1)]
 x = time.time()
 quick_sort(a)
 print("quick_sort", time.time() - x)
+print()
+
+a = [_ for _ in range(N, -1, -1)]
+x = time.time()
+quick_sort_insertion(a)
+print("quick_sort_insertion", time.time() - x)
