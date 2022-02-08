d013abc6ce70e284779bdd251e966251a2855967
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-08 14:01:08 +0200

    Planning to implement partition for quick sort.

--- quick_sort.py
+++ quick_sort.py
@@ -2,6 +2,10 @@ def quick_sort(sequence: list):
     pass
 
 
+def partition(sequence: list, low: int, high: int):
+    pass
+
+
 if __name__ == '__main__':
     a = [_ for _ in range(10, -1)]
     quick_sort(a)
