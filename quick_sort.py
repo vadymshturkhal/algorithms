ed6381248508bd8d8fd32dba1ec428ac5eb209ae
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-08 14:14:43 +0200

    Implemented partition.

--- quick_sort.py
+++ quick_sort.py
@@ -3,10 +3,28 @@ def quick_sort(sequence: list):
 
 
 def partition(sequence: list, low: int, high: int):
-    pass
+    i = low
+    j = high
+
+    while True:
+        while sequence[i] < sequence[low]:
+            i += 1
+            if i == high:
+                break
+
+        while sequence[low] < sequence[j]:
+            j -= 1
+            if j == low:
+                break
+
+        if i >= j:
+            break
+        sequence[i], sequence[j] = sequence[j], sequence[i]
+
+    sequence[low], sequence[j] = sequence[j], sequence[low]
 
 
 if __name__ == '__main__':
-    a = [_ for _ in range(10, -1)]
-    quick_sort(a)
+    a = [_ for _ in range(10, -1, -1)]
+    partition(a, 0, len(a) - 1)
     print(a)
