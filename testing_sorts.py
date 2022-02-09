53dc8883ecb8f32a5e544c2e08a2d0b7b7b111a4
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-09 16:05:00 +0200

    Tested all sorts. No recursion or index exceptions found.

--- testing_sorts.py
+++ testing_sorts.py
@@ -5,7 +5,7 @@ from quick_sort import quick_sort
 from quick_sort_insertion import quick_sort_insertion
 from insertion_sort import insertion_sort
 
-ARRAY_LENGTH = 900
+ARRAY_LENGTH = 5000
 
 SORTS = {
     'Timsort             ': list.sort,
@@ -16,9 +16,8 @@ SORTS = {
     'insertion_sort      ': insertion_sort,
 }
 
-for i in range(100):
-    for name, sort in SORTS.items():
-        array_to_sort = [_ for _ in range(ARRAY_LENGTH, 0, -1)]
-        start_time = time.time()
-        sort(array_to_sort)
-        print(name, time.time() - start_time)
+for name, sort in SORTS.items():
+    array_to_sort = [_ for _ in range(ARRAY_LENGTH, 0, -1)]
+    start_time = time.time()
+    sort(array_to_sort)
+    print(name, time.time() - start_time)
