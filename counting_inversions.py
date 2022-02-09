1e4e00c01ce76a7889c79ef8a7d44e3097dded01
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-09 15:42:24 +0200

    Planning to implement invesions count.

---
+++ counting_inversions.py
@@ -0,0 +1,9 @@
+def count_inversions(sequence: list) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    N = 10
+    a = [_ for _ in range(N, 0, -1)]
+    inversions_quantity = count_inversions(a)
+    print(inversions_quantity)
