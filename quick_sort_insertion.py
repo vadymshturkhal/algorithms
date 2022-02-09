47849b504847d3e9c050881b2b4720420ebb127e
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-09 18:00:44 +0200

    Removed standard partition.

--- quick_sort_insertion.py
+++ quick_sort_insertion.py
@@ -22,29 +22,6 @@ def quick_sort_insertion(sequence: list, low: int = None, high: int = None):
     quick_sort_insertion(sequence, right_bound + 1, high)
 
 
-def partition(sequence: list, low: int, high: int):
-    i = low + 1
-    j = high
-
-    while True:
-        while sequence[i] < sequence[low]:
-            i += 1
-            if i >= high:
-                break
-
-        while sequence[low] < sequence[j]:
-            j -= 1
-            if j <= low:
-                break
-
-        if i >= j:
-            break
-        sequence[i], sequence[j] = sequence[j], sequence[i]
-
-    sequence[low], sequence[j] = sequence[j], sequence[low]
-    return j
-
-
 if __name__ == '__main__':
     a = [_ for _ in range(20, -1, -1)]
     quick_sort_insertion(a)
