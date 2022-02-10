07283e6a87458ffa1ecb20be7d506797eb9c3c5d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-10 22:08:06 +0200

    Implemented counting inversions.

--- counting_inversions.py
+++ counting_inversions.py
@@ -1,9 +1,16 @@
 def count_inversions(sequence: list) -> int:
-    pass
+    """Max inversions quantity equals to n*(n - 1)/2."""
+
+    inversions_quantity = 0
+    for i in range(len(sequence)):
+        for j in range(i + 1, len(sequence)):
+            if sequence[i] > sequence[j]:
+                inversions_quantity += 1
+    return inversions_quantity
 
 
 if __name__ == '__main__':
     N = 10
-    a = [_ for _ in range(N, 0, -1)]
-    inversions_quantity = count_inversions(a)
-    print(inversions_quantity)
+    # a = [_ for _ in range(N, 0, -1)]
+    inversions = count_inversions(a)
+    print(inversions)
