f98d8b3e39168e452dcf7847d75e9f1c5dc5153a
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-10 21:49:56 +0200

    Add signature for is even.

--- sign_of_sequence.py
+++ sign_of_sequence.py
@@ -2,6 +2,10 @@ def sign_of_sequence(sequence: list) -> int:
     pass
 
 
+def is_even_permutation(sequence: list) -> bool:
+    pass
+
+
 if __name__ == '__main__':
     a = [7, 5, 1, 4, 2, 3, 6]
     sign = sign_of_sequence(a)
