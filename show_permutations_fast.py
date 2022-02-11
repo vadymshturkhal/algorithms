3f7e6b32f32f3900b7f087e78e9c1189439d52e6
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-11 18:33:34 +0200

    Implemented permutation from previous. Fixed bug with sequence[i].

--- show_permutations_fast.py
+++ show_permutations_fast.py
@@ -14,13 +14,19 @@ def show_permutations_anti_lexicographic_fast(sequence=None, length=None):
         for i in range(length):
             show_permutations_anti_lexicographic_fast(sequence, length - 1)
             if i < length - 1:
-                sequence[i], sequence[permutation_from_previous(length, i)] = sequence[permutation_from_previous(
-                    length, i)], sequence[i]
+                sequence[length - 1], sequence[permutation_from_previous(length - 1, i)] = sequence[permutation_from_previous(
+                    length - 1, i)], sequence[length - 1]
 
 
 def permutation_from_previous(length, index):
-    return 0
+    if length % 2 != 0 and length > 1:
+        if index < length - 1:
+            return index
+        else:
+            return length - 2
+    else:
+        return length - 1
 
 
 if __name__ == '__main__':
-    show_permutations_anti_lexicographic_fast(length=3)
+    show_permutations_anti_lexicographic_fast(length=4)
