17d52df0d157d4805b299c6f79976217ecda1d80
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-12 21:33:42 +0200

    Added some corner cases.

--- show_permutations_min_transposition.py
+++ show_permutations_min_transposition.py
@@ -1,6 +1,12 @@
-def show_permutations_min_transpositions():
-    pass
+def show_permutations_min_transpositions(sequence: list, length: int):
+    if sequence is None:
+        if length is None:
+            length = 3
+        sequence = [i + 1 for i in range(length)]
 
+    if length is None:
+        length = len(sequence)
 
+def init_
 if __name__ == '__main__':
     pass
