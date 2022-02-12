4bd63f3842438739190a4177262c15e08dd5b96d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-12 21:39:14 +0200

    Created some important lists.

--- show_permutations_min_transposition.py
+++ show_permutations_min_transposition.py
@@ -7,6 +7,9 @@ def show_permutations_min_transpositions(sequence: list, length: int):
     if length is None:
         length = len(sequence)
 
+    how_position = [1 for _ in range(length)]
+    is_element_moves_forward = [True for _ in range(length)]
+
 
 if __name__ == '__main__':
     pass
