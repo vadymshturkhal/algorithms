a030d0b8c32be5a344e094bbc0dc6be709c20a03
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-12 21:41:49 +0200

    Changed signature.

--- show_permutations_min_transposition.py
+++ show_permutations_min_transposition.py
@@ -1,4 +1,4 @@
-def show_permutations_min_transpositions(sequence: list, length: int):
+def show_permutations_min_transpositions(sequence: list = None, length: int = None):
     if sequence is None:
         if length is None:
             length = 3
@@ -10,6 +10,9 @@ def show_permutations_min_transpositions(sequence: list, length: int):
     how_position = [1 for _ in range(length)]
     is_element_moves_forward = [True for _ in range(length)]
 
+    how_position[length - 1] = 0
+    print(sequence)
+
 
 if __name__ == '__main__':
-    pass
+    show_permutations_min_transpositions(length=3)
