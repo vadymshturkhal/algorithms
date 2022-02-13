4a8d481689fe941b8825e1b9ddfaa3907084e183
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-13 14:44:49 +0200

    Added realization for two subsets.

--- generate_all_subsets.py
+++ generate_all_subsets.py
@@ -1,6 +1,21 @@
 def generate_all_subsets(power: int):
     """Gray code"""
-    pass
+    subset = [0 for _ in range(power)]
+    generated_subsets_quantity = 0
+
+    p = 0
+    j = 1
+    print(subset)
+    generated_subsets_quantity += 1
+
+    while j % 2 == 0:
+        j = j // 2
+        p += 1
+
+    if p <= power:
+        subset[p] = 1 - subset[p]
+
+    print(subset)
 
 
 if __name__ == '__main__':
