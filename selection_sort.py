83ca3cea93534108de86196252c35472b225799b
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-13 11:53:50 +0200

    Implemented selection sort.

--- selection_sort.py
+++ selection_sort.py
@@ -2,7 +2,10 @@ from random import shuffle
 
 
 def selection_sort(sequence: list) -> None:
-    pass
+    for i in range(len(sequence)):
+        for j in range(len(sequence)):
+            if sequence[i] < sequence[j]:
+                sequence[i], sequence[j] = sequence[j], sequence[i]
 
 
 if __name__ == '__main__':
