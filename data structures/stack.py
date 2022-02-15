8fa757f0d325638eb4ac2aace82d51d7f8394a40
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-15 13:16:10 +0200

    Implemented push method. Refactored node name.

--- data structures/stack.py
+++ data structures/stack.py
@@ -1,10 +1,21 @@
+from node import Node
+from typing import Any
+
+
 class Stack:
     """LIFO = "last in first out"""
-    def __init__(self):
-        pass
 
-    def push(self):
-        pass
+    def __init__(self):
+        self.__last_node = None
+
+    def push(self, item: Any):
+        old_first_node = self.__last_node
+
+        new_node = Node()
+        new_node.add_previous(old_first_node)
+        new_node.add_item(item)
+
+        self.__last_node = new_node
 
     def pop(self):
         pass
