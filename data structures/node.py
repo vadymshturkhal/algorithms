74c7a5d808dc44924dee93c9e8f2e57a91c4704e
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-15 10:54:15 +0200

    Implemented interface for Node.

--- data structures/node.py
+++ data structures/node.py
@@ -1,13 +1,16 @@
-from typing import Generic, ParamSpec, TypeVar
+from typing import Any
 
 
 class Node:
     """Node for data structures"""
+
     def __init__(self):
+        self.item = None
+        self.forward = None
+        self.previous = None
+
+    def add_item(self, item: Any) -> None:
         pass
 
-    def item(self):
-        pass
-
-    def next(self, n: 'Node'):
+    def add_next(self, n: 'Node') -> None:
         pass
