9bfb2915ad7c6bbb3eb81e5187216a8318c6e4df
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-15 10:39:24 +0200

    Planning to implement stack.

---
+++ data structures/stack.py
@@ -0,0 +1,17 @@
+class Stack:
+    def __init__(self):
+        pass
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def __len__(self):
+        return 0
+
+
+if __name__ == '__main__':
+    s = Stack()
+    print(len(s))
