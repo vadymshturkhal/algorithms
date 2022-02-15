b8a95550b65359b41bdcde39b4596078978aef53
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-15 10:57:10 +0200

    Implemented Node.

--- data structures/node.py
+++ data structures/node.py
@@ -5,12 +5,19 @@ class Node:
     """Node for data structures"""
 
     def __init__(self):
-        self.item = None
-        self.forward = None
-        self.previous = None
+        self.__item = None
+        self.__forward = None
+        self.__previous = None
 
     def add_item(self, item: Any) -> None:
-        pass
+        self.__item = item
+
+    def retrieve_item(self) -> Any:
+        return self.__item
 
     def add_next(self, n: 'Node') -> None:
-        pass
+        self.__forward = n
+
+    def add_previous(self, n: 'Node') -> None:
+        self.__previous = n
+
