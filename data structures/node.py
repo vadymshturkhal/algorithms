e98f5ac91c261bc7d4cf9d99a8d0ccd6d7f28f82
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-15 10:47:43 +0200

    Created node for data structures.

---
+++ data structures/node.py
@@ -0,0 +1,13 @@
+from typing import Generic, ParamSpec, TypeVar
+
+
+class Node:
+    """Node for data structures"""
+    def __init__(self):
+        pass
+
+    def item(self):
+        pass
+
+    def next(self, n: 'Node'):
+        pass
