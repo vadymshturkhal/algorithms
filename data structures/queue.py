93c83cf8ae4ad3472e9ded72c2c52b10473127ea
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-16 12:42:02 +0200

    Implemented enqueue method.

--- data structures/queue.py
+++ data structures/queue.py
@@ -1,12 +1,25 @@
+from node import Node
 from typing import Any
 
 
 class Queue:
     def __init__(self):
+        self.__first_node = None
+        self.__last_node = None
         self.__length = 0
 
-    def enqueue(self, item: Any):
-        pass
+    def enqueue(self, item: Any) -> None:
+        self.__length += 1
+
+        new_node = Node()
+        new_node.item = item
+        if self.__last_node is None:
+            self.__first_node = new_node
+            self.__last_node = new_node
+            return
+
+        self.__last_node.next_node = new_node
+        self.__last_node = new_node
 
     def dequeue(self):
         pass
