4cad58fbc9c6a7d3d10520def75be7f9596e1081
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-17 00:47:19 +0200

    Implemented interface for Priority Queue.

--- data structures/priority_queue.py
+++ data structures/priority_queue.py
@@ -1,6 +1,18 @@
+from typing import Any
+
+
 class PriorityQueue:
     def __init__(self):
+        self.__length = 0
+
+    def insert(self, key: Any) -> None:
         pass
+
+    def delete_max(self) -> Any:
+        pass
+
+    def __len__(self):
+        return self.__length
 
 
 if __name__ == '__main__':
