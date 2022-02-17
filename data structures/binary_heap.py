20d98df30187c1e2792e2c8008b7ccb26829a9dc
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-17 12:13:54 +0200

    Created interface for a binary heap.

---
+++ data structures/binary_heap.py
@@ -0,0 +1,22 @@
+from typing import Any, Union
+
+
+class BinaryHeap:
+    def __init__(self):
+        self.__items = []
+
+    def insert(self, item: Any) -> None:
+        pass
+
+    def del_max(self) -> Union[Any, None]:
+        pass
+
+    def __swim(self):
+        pass
+
+    def __sink(self):
+        pass
+
+
+if __name__ == '__main__':
+    bh = BinaryHeap()
