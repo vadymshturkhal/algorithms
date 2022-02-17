f55d61dc2d6b1ef203c1e09b848afc1a423557c3
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-17 11:15:13 +0200

    Implemented unordered priority queue.

--- data structures/priority_queue_unordered.py
+++ data structures/priority_queue_unordered.py
@@ -1,20 +1,39 @@
-from typing import Any
+from typing import Any, Union
 
 
 class UnorderedMaxPriorityQueue:
     def __init__(self):
-        self.__length = 0
         self.__items = []
 
     def insert(self, item: Any) -> None:
         self.__items.append(item)
 
-    def delete_max(self) -> Any:
-        pass
+    def delete_max(self) -> Union[Any, None]:
+        if len(self.__items) == 0:
+            return
+
+        length = len(self.__items)
+        max_item_index = 0
+        for current_index in range(1, length):
+            current_item = self.__items[current_index]
+
+            if current_item > self.__items[max_item_index]:
+                max_item_index = current_index
+
+        self.__items[max_item_index], self.__items[length - 1] = self.__items[length - 1], self.__items[max_item_index]
+
+        return self.__items.pop()
 
     def __len__(self):
-        return self.__length
+        return len(self.__items)
 
 
 if __name__ == '__main__':
-    pass
+    unordered_max_pq = UnorderedMaxPriorityQueue()
+    ITEMS_LENGTH = 10
+    for i in range(ITEMS_LENGTH, 0, -1):
+        unordered_max_pq.insert(i)
+
+    print(unordered_max_pq.delete_max())
+    print(unordered_max_pq.delete_max())
+    print(unordered_max_pq.delete_max())
