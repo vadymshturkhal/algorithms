a5a44fc09204643d7c0845a9d92dcf08d6fe7dd4
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-17 13:53:32 +0200

    Planning to implement heap sort.

---
+++ algorithms/heap_sort.py
@@ -0,0 +1,10 @@
+def heap_sort(sequence: list) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    SEQUENCE_LENGTH = 10
+    a = [_ for _ in range(SEQUENCE_LENGTH, 0, -1)]
+    print(a)
+    heap_sort(a)
+    print(a)
