399b12336ab2d82671432cfc90fd13dd5aab1305
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-17 12:19:13 +0200

    Implemented insert and swim methods for binary heap.

--- data structures/binary_heap.py
+++ data structures/binary_heap.py
@@ -6,17 +6,29 @@ class BinaryHeap:
         self.__items = []
 
     def insert(self, item: Any) -> None:
-        pass
+        self.__items.append(item)
+        self.__swim(len(self.__items) - 1)
 
     def del_max(self) -> Union[Any, None]:
         pass
 
-    def __swim(self):
-        pass
+    def __swim(self, index):
+        items = self.__items
+        while index > 1 and items[index // 2] > items[index]:
+            items[index], items[index // 2] = items[index // 2], items[index]
+            index //= 2
 
     def __sink(self):
         pass
 
+    def __len__(self):
+        return len(self.__items)
+
 
 if __name__ == '__main__':
+    HEAP_ITEMS_QUANTITY = 10
     bh = BinaryHeap()
+    for i in range(HEAP_ITEMS_QUANTITY):
+        bh.insert(i)
+
+    print(len(bh))
