86875a99ebc2928ccc321ef591e86c6c2f3c3718
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-18 18:35:00 +0200

    Implemented naive heap sort. Need fix speed.

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,10 +1,20 @@
+from algorithms.knuth_shuffle import knuth_shuffle
+from data_structures.binary_heap import BinaryHeap
+
+
 def heap_sort(sequence: list) -> None:
-    pass
+    bh = BinaryHeap(return_max=False)
+    for item in sequence:
+        bh.insert(item)
+
+    for i in range(len(sequence)):
+        sequence[i] = bh.del_element()
 
 
 if __name__ == '__main__':
     SEQUENCE_LENGTH = 10
     a = [_ for _ in range(SEQUENCE_LENGTH, 0, -1)]
+    knuth_shuffle(a)
     print(a)
     heap_sort(a)
     print(a)
