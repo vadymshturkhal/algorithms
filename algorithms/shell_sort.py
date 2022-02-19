872dc3e4938a3292d16e3228a29fc3eff6a2b6fd
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-19 20:54:21 +0200

    Added comments.

--- algorithms/shell_sort.py
+++ algorithms/shell_sort.py
@@ -3,10 +3,12 @@ from random import shuffle
 
 def shell_sort(sequence: list) -> None:
     h = 1
+    # Used 3x + 1 increment sequence.
     while h < len(sequence) / 3:
         h = 3 * h + 1
 
     while h >= 1:
+        # Insertion sort,
         for i in range(len(sequence)):
             for j in range(i, h - 1, -h):
                 if sequence[j] < sequence[j - h]:
