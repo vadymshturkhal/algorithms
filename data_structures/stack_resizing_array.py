c8a5385d0d9485d368a9b12a8c31a6d8275896c7
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-21 22:36:25 +0200

    Created signature for Stack with resizing array.

---
+++ data_structures/stack_resizing_array.py
@@ -0,0 +1,16 @@
+class StackResizingArray:
+    def __init__(self):
+        pass
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def __len__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
