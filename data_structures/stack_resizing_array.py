f26ca9ebe881bcb8aac3c8377a98bcbb159bb81c
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-21 22:57:47 +0200

    Implemented pop for stack.

--- data_structures/stack_resizing_array.py
+++ data_structures/stack_resizing_array.py
@@ -14,11 +14,20 @@ class StackResizingArray:
         self.__length += 1
 
     def pop(self) -> Union[None, Any]:
-        pass
+        length = self.__length - 1
+
+        temp_item = self.__data[length]
+        self.__data[length] = None
+        self.__length -= 1
+
+        if self.__length > 0 and self.__length == len(self.__data) // 4:
+            self.__resize_array(len(self.__data) // 2)
+
+        return temp_item
 
     def __resize_array(self, capacity):
         temp_array = [None] * capacity
-        for i in range(len(self.__data)):
+        for i in range(self.__length):
             temp_array[i] = self.__data[i]
         self.__data = temp_array
 
@@ -27,10 +36,14 @@ class StackResizingArray:
 
 
 if __name__ == '__main__':
-    STACK_LENGTH = 20
+    STACK_LENGTH = 10
     s = StackResizingArray()
 
     for i in range(1, STACK_LENGTH + 1):
         s.push(i)
 
+    print("Stack length =", len(s))
+
+    for i in range(1, STACK_LENGTH + 1):
+        print(s.pop())
     print("Stack length =", len(s))
