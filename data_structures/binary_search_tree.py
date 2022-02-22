1abc3263819affe7cc21d47cdec354570cf04fdb
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-22 17:07:31 +0200

    Added comment for binary search tree.

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -1,7 +1,15 @@
 from typing import Any, Union
+from data_structures.node import Node
 
 
 class BinarySearchTree:
+    """
+        Symmetric order.
+        Each node has a key, and every node’s key is:
+            1. Larger than all keys in its left subtree;
+            2.Smaller than all keys in its right subtree.
+    """
+
     def __init__(self):
         self.__root = None
 
