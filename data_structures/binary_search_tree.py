04cb8d32042dde28de4c4fd2b3880ba12a3588ca
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-22 17:03:26 +0200

    Added interface for binary search tree.

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -1,3 +1,15 @@
+from typing import Any, Union
+
+
 class BinarySearchTree:
     def __init__(self):
+        self.__root = None
+
+    def put(self, item: Any) -> None:
+        pass
+
+    def get(self) -> Union[None, Any]:
+        pass
+
+    def delete(self) -> None:
         pass
