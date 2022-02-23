02ff73fca05296ba22941d0778ce01246d28fcb1
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-02-23 19:55:41 +0200

    Implemented get method at binary_search_tree.

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -7,7 +7,7 @@ class BinarySearchTree:
         Symmetric order.
         Each node has a key, and every node’s key is:
             1. Larger than all keys in its left subtree;
-            2.Smaller than all keys in its right subtree.
+            2. Smaller than all keys in its right subtree.
     """
 
     def __init__(self):
@@ -16,8 +16,18 @@ class BinarySearchTree:
     def put(self, item: Any) -> None:
         pass
 
-    def get(self) -> Union[None, Any]:
-        pass
+    def get(self, key) -> Union[None, Any]:
+        current_node = self.__root
+
+        while current_node is not None:
+            if current_node.key < key:
+                current_node = current_node.previous
+            elif current_node.key > key:
+                current_node = current_node.next_node
+            else:
+                return current_node.item
+
+        return None
 
     def delete(self) -> None:
         pass
