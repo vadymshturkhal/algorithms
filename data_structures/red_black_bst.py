cf09b4c0786f607aa91e484eda26e842530edc82
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-03-05 18:32:45 +0200

    Added get to Red-Black.

--- data_structures/red_black_bst.py
+++ data_structures/red_black_bst.py
@@ -1,6 +1,24 @@
+from typing import Union, Any
+from data_structures.node import Node
+
+
 class RedBlackBST:
     def __init__(self):
-        pass
+        self.__root = None
+
+    def get(self, key) -> Union[None, Any]:
+        """Search is the same as for elementary BST (ignore color)"""
+        current_node = self.__root
+
+        while current_node is not None:
+            if key < current_node.key:
+                current_node = current_node.left
+            elif key > current_node.key:
+                current_node = current_node.right
+            else:
+                return current_node.item
+
+        return None
 
 
 if __name__ == '__main__':
