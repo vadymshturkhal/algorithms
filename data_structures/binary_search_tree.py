d2e6bdf1de317f491defacaf87576161a9246fbb
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-03-06 10:54:37 +0200

    Refactored BST.

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -15,96 +15,38 @@ class BinarySearchTree:
 
     def __init__(self):
         self.__root = None
-        self.__count = 0
 
-    def put(self, key: Any, item: Any) -> None:
-        self.__root = self.__put(self.__root, key, item)
+    def put(self, key: Any, value: Any) -> None:
+        self.__root = self.__put(self.__root, key, value)
 
     def get(self, key) -> Union[None, Any]:
         current_node = self.__root
 
         while current_node is not None:
             if key < current_node.key:
-                current_node = current_node.previous
+                current_node = current_node.left
             elif key > current_node.key:
-                current_node = current_node.next_node
+                current_node = current_node.right
             else:
                 return current_node.item
 
         return None
 
-    def size(self):
-        return self.__size(self.__root)
-
-    def rank(self, key: Any) -> int:
-        return self.__rank(key, self.__root)
-
     def delete(self) -> None:
         pass
 
-    def max(self) -> Node:
-        pass
-
-    def min(self) -> Node:
-        pass
-
-    def floor(self, key: Any) -> Any:
-        """Largest key that is less than n"""
-        temp = self.__floor(self.__root, key)
-
-        if temp is None:
-            return None
-
-        return temp.key
-
-    def ceiling(self) -> Node:
-        pass
-
-    def __put(self, node: Node, key: Any, item: Any) -> Node:
+    def __put(self, node: Node, key: Any, value: Any) -> Node:
         if node is None:
-            return Node(key, item)
+            return Node(key, value)
 
         if key < node.key:
-            node.left = self.__put(node.left, key, item)
+            node.left = self.__put(node.left, key, value)
         elif key > node.key:
-            node.right = self.__put(node.right, key, item)
+            node.right = self.__put(node.right, key, value)
         else:
-            node.item = item
-
-        node.count = 1 + self.__size(node.left) + self.__size(node.right)
+            node.item = value
 
         return node
-
-    def __floor(self, n: Node, key: Any) -> Union[None, Node]:
-        if n is None:
-            return None
-
-        if key == n.key:
-            return n
-
-        if key < n.key:
-            return self.__floor(n.left, key)
-
-        temp = self.__floor(n.right, key)
-
-        return n if temp is None else temp
-
-    def __size(self, n: Node) -> int:
-        if n is None:
-            return 0
-        return n.count
-
-    def __rank(self, key: Any, n: Node) -> int:
-        if n is None:
-            return 0
-
-        if key < n.key:
-            return self.__rank(key, n.left)
-
-        if key > n.key:
-            return 1 + self.__size(n.left) + self.__rank(key, n.right)
-
-        return self.__size(n.left)
 
 
 if __name__ == '__main__':
