f9bdee320ad522f2e0dd7ad4764dec89d9c92d6f
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-08 15:45:33 +0300

    Created interface for linked list.

---
+++ data_structures/linked_list.py
@@ -0,0 +1,27 @@
+from data_structures.node import Node
+
+class LinkedList:
+    def __init__(self) -> None:
+        pass
+
+    def search(self) -> int:
+        """Returns index or -1"""
+        pass
+
+    def insert(self, value) -> None:
+        pass
+
+
+    def delete(self, value):
+        pass
+
+    def __len__(self):
+        pass
+
+
+if __name__ == '__main__':
+    LIST_SIZE = 5
+    l_l = LinkedList()
+
+    for i in range(1, LIST_SIZE + 1):
+        l_l.insert(i)
