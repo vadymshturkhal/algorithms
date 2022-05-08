3e10247c7fd5ebc935db277ae27670fef2a46755
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-05-08 16:09:11 +0300

    Implemented insert, str, search.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,22 +1,54 @@
 from data_structures.node import Node
 
+
 class LinkedList:
     def __init__(self) -> None:
-        pass
+        self.__first = None
+        self.__current = None
+        self.__length = 0
 
-    def search(self) -> int:
-        """Returns index or -1"""
-        pass
+    def search(self, value) -> [None, Node]:
+        """Returns Node or None"""
+        cur = self.__first
+
+        while cur is not None:
+            if cur.item == value:
+                return cur
+
+            cur = cur.next_node
+
+        return
 
     def insert(self, value) -> None:
-        pass
+        if self.is_empty():
+            self.__first = Node(value=value)
+            self.__current = self.__first
+            self.__length += 1
+            return
 
+        current = Node(value=value)
+        current.previous = self.__current
+        self.__current.next_node = current
+        self.__current = current
 
     def delete(self, value):
         pass
 
+    def is_empty(self):
+        return self.__length == 0
+
     def __len__(self):
-        pass
+        return self.__length
+
+    def __str__(self):
+        cur = self.__first
+
+        values = []
+        while cur is not None:
+            values.append(cur.item)
+            cur = cur.next_node
+
+        return f'{values}'
 
 
 if __name__ == '__main__':
@@ -25,3 +57,7 @@ if __name__ == '__main__':
 
     for i in range(1, LIST_SIZE + 1):
         l_l.insert(i)
+
+    n = l_l.search(3)
+    n.item = 11
+    print(l_l)
