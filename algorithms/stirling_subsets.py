6e14e1a48c5a345315a73db73f648b887750c48d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-06-29 16:12:15 +0300

    Trying to implement.

--- algorithms/stirling_subsets.py
+++ algorithms/stirling_subsets.py
@@ -1,11 +1,27 @@
 def stirling_subsets(length: int):
-    elements = [_ for _ in range(1, length + 1)]
+    start = 1
+    elements = [_ for _ in range(start, length + start)]
+    is_move_forward = [True for _ in range(length)]
     print(elements)
 
     cursor = length - 1
 
     # if cursor move forward 
     # then move forward (123)(4)
+    if is_move_forward[cursor]:
+        element = elements[cursor]
+    
+    print(elements[:cursor], [element])
+    # print(cursor, cursor + start)
+    while cursor > 0 and is_move_forward[cursor] and elements[cursor] == (cursor + start):
+        # print(cursor, is_move_forward[cursor])
+        is_move_forward[cursor] = not is_move_forward
+        # print(cursor, is_move_forward[cursor])
+
+        cursor -= 1
+
+    # print(cursor)
+
 
 if __name__ == '__main__':
     SEQUENCE_LENGTH = 4
@@ -14,14 +30,14 @@ if __name__ == '__main__':
         Must be:
             (1234)
             (123)(4)
-            (12)(3)(4
+            (12)(3)(4)
             (12)(34)
             (124)(3)
-            (14)(2)(3
-            (1)(24)(3
-            (1)(2)(34
-            (1)(2)(3) 4)
-            (1)(23)(4
+            (14)(2)(3)
+            (1)(24)(3)
+            (1)(2)(34)
+            (1)(2)(3)(4)
+            (1)(23)(4)
             (1)(234)
             (14)(23)
             (134)(2)
