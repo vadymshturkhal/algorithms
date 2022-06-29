7d55b0f0b4ff0e7e357f15dd883e3f0922a9f03a
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-06-29 23:19:45 +0300

    Reforged stirling.

--- algorithms/stirling_subsets.py
+++ algorithms/stirling_subsets.py
@@ -8,42 +8,20 @@ def stirling_subsets(elements, to_print=None, default_start=1):
         elements = [_ for _ in range(default_start, length + default_start)]
 
     if len(elements) == 2:
-        if to_print:
-            print(tuple(to_print), tuple(elements))
-            print(tuple(to_print), f'({elements[0]}) ({elements[1]})')
-        else:
-            print(tuple(elements))
-            print(f'({elements[0]}) ({elements[1]})')
-        return
-    
-    if len(elements) <= 1:
-        return
-
-    for i in range(length // 2 + 1):
-        i = length - i
-        comb_gen = comb_visual_generators(elements, i)
-
-        for current_combination in comb_gen:
-            diff = set(elements).difference(current_combination)
-
-            # default
-            # print(current_combination, diff)
-            if len(diff) == 0:
-                print(current_combination)
-            elif len(diff) == 1:
-                print(current_combination, f'({diff.pop()})');
-            else:  # len(diff) >= 2
-                stirling_subsets(list(diff), to_print=current_combination)
-
-            # for avoid 
-            # (1)(2)
-            # (2)(1)
-            if len(elements) <= 2:
-                break
+        # if to_print:
+        #     print(tuple(to_print), tuple(elements))
+        #     print(tuple(to_print), f'({elements[0]}) ({elements[1]})')
+        # else:
+        #     print(tuple(elements))
+        #     print(f'({elements[0]}) ({elements[1]})')
+        all_partitions = (set(elements), (set([elements[0]]), set([elements[1]])))
+        for partition in all_partitions:
+            print(partition)
+        return all_partitions
 
 
 if __name__ == '__main__':
-    SEQUENCE_LENGTH = 4
+    SEQUENCE_LENGTH = 2
 
     """
         Must be:
