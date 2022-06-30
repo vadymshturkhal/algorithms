3d15a3864194a6ce6103f7594d188a4f8efe3e04
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-06-30 10:49:34 +0300

    Implementing.

--- algorithms/stirling_subsets.py
+++ algorithms/stirling_subsets.py
@@ -8,15 +8,17 @@ def stirling_subsets(elements, to_union=None, default_start=1):
         elements = [_ for _ in range(default_start, length + default_start)]
 
     if len(elements) <= 1:
-        x = [tuple(elements)]
-        if to_union is not None:
-            to_return = invariant(x, {to_union})
-            return to_return
-        return x
+        # x = tuple(elements)
+        # if to_union is not None:
+        #     to_return = invariant(x, {to_union})
+        #     return to_return
+        return (set(elements),)
 
-    x = stirling_subsets(elements[:len(elements) - 1], elements[-1])
-    
-    return x
+    to_union = elements[-1]
+    x = stirling_subsets(elements[:len(elements) - 1], to_union)
+    to_return = invariant(x, {to_union})
+    return to_return
+    # return x
     # if len(elements) == 2:
     #     # if to_print:
     #     #     print(tuple(to_print), tuple(elements))
@@ -30,15 +32,45 @@ def stirling_subsets(elements, to_union=None, default_start=1):
     #     return all_partitions
 
 
-def invariant(partitions: list, to_union):
-    unions = []
+def invariant(partitions: list, to_union, unions=None):
+    if unions is None:
+        unions = []
+
     for part in partitions:
-        for p in part:
-            t = set([p]).union(to_union)
-            t1 = (set([p]), to_union)
-            unions.append(t)
-            unions.append(t1)
-    return tuple(unions)
+        if type(part) == set:
+
+            x = union_sets(part, to_union)
+            x1 = add_to_set(part, to_union)
+            # print(x, x1)
+            unions.append((x, x1))
+            # return (x, x1)
+        else:
+            x = invariant(part, to_union, unions)
+            print(part, to_union)
+
+            # return x
+    # print(unions)
+    return unions
+
+def union_sets(partition, to_union):
+    """
+        Example:
+            partition: {1, 2, 3}
+            to_union: {4}
+            result: ({1, 2, 3, 4},)
+    """
+    # print((partition.union(to_union), ))
+    return (partition.union(to_union), )
+
+def add_to_set(partition, to_add):
+    """
+        Example:
+            partition: {1, 2, 3}
+            to_add: {4}
+            result: ({1, 2, 3}, {4})
+    """
+    # print((partition, to_add))
+    return (partition, to_add)
 
 if __name__ == '__main__':
     SEQUENCE_LENGTH = 3
@@ -63,5 +95,6 @@ if __name__ == '__main__':
     """
     # stirling_subsets(SEQUENCE_LENGTH)
     x = stirling_subsets(SEQUENCE_LENGTH)
-    for i in x:
-        print(*i)
+    print(x)
+    # for i in x:
+        # print(*i)
