7bb61accb764672585b03f6531521dbb79caa72f
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-06-30 19:30:49 +0300

    Integrated frozensets.

--- algorithms/stirling_subsets.py
+++ algorithms/stirling_subsets.py
@@ -1,4 +1,3 @@
-from re import sub
 from comb_visual_generators import comb_visual_generators
 
 def stirling_subsets(elements, to_union=None, default_start=1):
@@ -9,46 +8,40 @@ def stirling_subsets(elements, to_union=None, default_start=1):
         elements = [_ for _ in range(default_start, length + default_start)]
 
     if len(elements) <= 1:
-        # return tuple of tuples for invariant
-        return ((set(elements),),)
+        elements = {frozenset(elements)}
+        return elements
 
-    to_union = elements.pop()
-    x = stirling_subsets(elements, to_union)
-    to_return = invariant(x, {to_union})
-    return to_return
+    # must be a set
+    element_to_union = frozenset([elements.pop()])
+    subset = stirling_subsets(elements, element_to_union)
+    unioned = invariant(subset, element_to_union)
+    return unioned
 
-def invariant(partitions: list, to_union, unions=None):
-    if unions is None:
-        unions = []
+def invariant(partitions: list, to_union):
+    new_set = set()
+    for partition in partitions:
+        # print(partition)
+        new_set.add(union_sets(partition, to_union))
+        new_set.add(add_to_set(partition, to_union))
+    return new_set
 
-    for part in partitions:
-        if type(part) == list:
-            invariant(part, to_union, unions)
-        else:  # type(part) == tuple
-            # print('part', part, 'to_union', to_union)
-            add = add_to_set(part, to_union)
-            union = union_sets(part, to_union)
-            result = [union, add]
-            unions.append(result)
-    return unions
-
-def union_sets(subset: set, to_union: set) -> tuple:
+def union_sets(subset: set, to_union: set) -> frozenset:
     """
         Example:
             subset: {1, 2, 3}
             to_union: {4}
             result: ({1, 2, 3, 4}, )
     """
-    return (subset.union(to_union), )
+    return subset.union(to_union)
 
-def add_to_set(subset: set, to_add: set) -> tuple:
+def add_to_set(subset: frozenset, to_add: frozenset) -> frozenset:
     """
         Example:
             subset: {1, 2, 3}
             to_add: {4}
             result: ({1, 2, 3}, {4})
     """
-    return (subset, to_add)
+    return frozenset([subset, to_add])
 
 if __name__ == '__main__':
     SEQUENCE_LENGTH = 2
@@ -71,15 +64,5 @@ if __name__ == '__main__':
             (13)(24)
             (13)(2)(4)
     """
-    # stirling_subsets(SEQUENCE_LENGTH)
-    # x = stirling_subsets(SEQUENCE_LENGTH)
-    # print(x)
-    # for i in x:
-        # print(*i)
-
-    print(union_sets({1, 2}, {3}))
-    print(union_sets({1}, {3}))
-    print(union_sets({2}, {3}))
-    # print(union_sets({1, 2}, {3}))
-    # print(union_sets({1, 2}, {3}))
-    # print(add_to_set({1, 2}, {3}))
+    all_subsets = stirling_subsets(SEQUENCE_LENGTH)
+    print(all_subsets)
