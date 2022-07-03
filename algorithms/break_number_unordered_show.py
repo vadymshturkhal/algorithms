122b27fb3b252a42d22e70c9a94c82f85fbca71f
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-07-03 10:13:12 +0300

    Finished break unordered show.

--- algorithms/break_number_unordered_show.py
+++ algorithms/break_number_unordered_show.py
@@ -12,7 +12,7 @@ def break_unorder_show(num):
         if unique_terms[cursor] == 1:
             current_sum += times_appear[cursor]
             cursor -= 1
-        
+
         current_sum += unique_terms[cursor]
         times_appear[cursor] = times_appear[cursor] - 1
 
@@ -20,23 +20,21 @@ def break_unorder_show(num):
 
         if times_appear[cursor] > 0:
             cursor += 1
-        
-        unique_terms[cursor] = new_term
-        times_appear[cursor] = current_sum // new_term
+
+        update_at_cursor(unique_terms, cursor, new_term)
+        update_at_cursor(times_appear, cursor, current_sum // new_term)
 
         new_term = current_sum % new_term
+
+        clean_to_cursor(unique_terms, cursor)
+        clean_to_cursor(times_appear, cursor)
 
         # add last term which equals to new_term
         if new_term != 0:
             cursor += 1
 
-            if cursor >= len(unique_terms):
-                unique_terms.append(new_term)
-            if cursor >= len(times_appear):
-                times_appear.append(1)
-
-            unique_terms[cursor] = new_term
-            times_appear[cursor] = 1
+            update_at_cursor(unique_terms, cursor, new_term)
+            update_at_cursor(times_appear, cursor, 1)
 
         yield get_terms(unique_terms, times_appear)
 
@@ -51,10 +49,41 @@ def get_terms(unique_terms, times_appear):
     return terms
 
 
+def update_at_cursor(members, cursor, to_add):
+    if len(members) <= cursor:
+        members.append(to_add)
+    
+    members[cursor] = to_add
+
+def clean_to_cursor(to_clean, cursor):
+    # to_clean = to_clean[:cursor + 1]
+
+    len_to_erase = len(to_clean) - cursor - 1
+    for i in range(len_to_erase):
+        to_clean.pop()
+
 
 if __name__ == '__main__':
     NUMBER = 7
-    x = break_unorder_show(NUMBER)
+    all_breakdowns = break_unorder_show(NUMBER)
 
-    for i in x:
-        print(i)
+    for breakdown in all_breakdowns:
+        print(*breakdown)
+
+    """
+    7
+    6 1
+    5 2
+    5 1 1
+    4 3
+    4 2 1
+    4 1 1 1
+    3 3 1
+    3 2 2
+    3 2 1 1
+    3 1 1 1 1
+    2 2 2 1
+    2 2 1 1 1
+    2 1 1 1 1 1
+    1 1 1 1 1 1 1
+    """
