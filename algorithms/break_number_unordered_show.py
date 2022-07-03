b7d192b7b4f9fc4820356212f5095008a36b8a69
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-07-03 09:47:23 +0300

    Implemented break unordered show with bug.

--- algorithms/break_number_unordered_show.py
+++ algorithms/break_number_unordered_show.py
@@ -5,7 +5,7 @@ def break_unorder_show(num):
     yield get_terms(unique_terms, times_appear)
 
     cursor = 0
-    while unique_terms[cursor] > 1:
+    while unique_terms[0] > 1:
         current_sum = 0
 
         # delete terms which equals to 1
@@ -24,16 +24,28 @@ def break_unorder_show(num):
         unique_terms[cursor] = new_term
         times_appear[cursor] = current_sum // new_term
 
-        print(current_sum, unique_terms, times_appear, new_term, cursor)
-        break
+        new_term = current_sum % new_term
 
+        # add last term which equals to new_term
+        if new_term != 0:
+            cursor += 1
+
+            if cursor >= len(unique_terms):
+                unique_terms.append(new_term)
+            if cursor >= len(times_appear):
+                times_appear.append(1)
+
+            unique_terms[cursor] = new_term
+            times_appear[cursor] = 1
+
+        yield get_terms(unique_terms, times_appear)
 
 def get_terms(unique_terms, times_appear):
     terms = []
     for i in range(len(times_appear)):
         times = times_appear[i]
 
-        for i in range(times):
+        for j in range(times):
             terms.append(unique_terms[i])
     
     return terms
