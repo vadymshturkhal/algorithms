b3c4344a22e19218b381a28def1b1a4256b6d4f8
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-07-04 16:53:45 +0300

    Graph based on dict.

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -2,12 +2,14 @@ from copy import deepcopy
 
 class Graph:
     """
-        Used incidence list.
-        Edge is list with tuples with ints.
+        Used dict.
+        {1: [2, 3]} means edges: 
+            (1, 2)
+            (1, 3)
     """
 
     def __init__(self, edges: list = None) -> None:
-        self.__edges = []
+        self.__edges = {}
         self.add_edges(edges)
 
     def add_edges(self, edges: list = None, *, is_oriented: bool = True) -> None:
@@ -17,27 +19,27 @@ class Graph:
         for edge in edges:
             from_, to_ = edge
 
-            self.__init_len_of_edges(from_)
+            if self.__edges.get(from_) is None:
+                self.__edges[from_] = []
+
             self.__edges[from_].append(to_)
 
             if not is_oriented:
-                self.__init_len_of_edges(to_)
+                if self.__edges.get(to_) is None:
+                    self.__edges[to_] = []
+
                 self.__edges[to_].append(from_)
-    
+
     def get_neighbours(self, vertex: int) -> list:
         """Returned copy of vertecies"""
-        if vertex >= len(self.__edges):
-            return []
+        if self.__edges.get(vertex) is None:
+            return
 
         return deepcopy(self.__edges[vertex])
 
 
     def show_graph(self):
         print(self.__edges)
-
-    def __init_len_of_edges(self, required_len: int):
-        while required_len >= len(self.__edges):
-            self.__edges.append([])
 
 
 if __name__ == '__main__':
@@ -64,3 +66,5 @@ if __name__ == '__main__':
     g = Graph()
     g.add_edges(edges, is_oriented=False)
     g.show_graph()
+
+    print(g.get_neighbours(10))
