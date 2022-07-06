480f83fc5a0a8ec9dbfc253e5ef0625ea38bf589
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-07-06 16:07:47 +0300

    Euler doesn't work correct.

--- algorithms/graph_euler_path.py
+++ algorithms/graph_euler_path.py
@@ -2,7 +2,34 @@ from data_structures.graph import Graph
 
 
 def euler_path(graph: Graph, start_vertex: int) -> list:
+    stack = []
     euler_path_sequence = []
+
+    vertices = {}
+
+    stack.append(start_vertex)
+
+    while len(stack):
+        top_vertex = stack[-1]
+
+        if top_vertex not in vertices:
+            vertices[top_vertex] = graph.get_neighbours(top_vertex)
+
+        neighbours = vertices[top_vertex]
+
+        if len(neighbours):
+            first_neighbour = neighbours[0]
+
+            if first_neighbour not in vertices:
+                vertices[first_neighbour] = graph.get_neighbours(first_neighbour)
+
+            vertices[first_neighbour].remove(top_vertex)
+            vertices[top_vertex].remove(first_neighbour)
+
+            stack.append(first_neighbour)
+        else:
+            euler_path_sequence.append(stack.pop())
+
     return euler_path_sequence
 
 
