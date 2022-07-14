79e3819e73322f6ec016c7edd4fe0b8bddfe09ad
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-07-14 09:54:11 +0300

    Reviewed binary heap.

--- data_structures/binary_heap.py
+++ data_structures/binary_heap.py
@@ -2,8 +2,8 @@ from typing import Any, Union
 
 
 class BinaryHeap:
-    def __init__(self, return_max=False):
-        self.__items = []
+    def __init__(self, *, return_max=False):
+        self.__items = [None]
         self.__return_max = return_max
 
     def insert(self, item: Any) -> None:
@@ -13,13 +13,16 @@ class BinaryHeap:
     def del_element(self) -> Union[Any, None]:
         items = self.__items
 
-        if len(items) <= 0:
+        if len(items) == 2:
+            return items.pop()
+
+        if len(items) < 2:
             return
 
-        items[0], items[len(items) - 1] = items[len(items) - 1], items[0]
+        items[1], items[len(items) - 1] = items[len(items) - 1], items[1]
         max_item = items.pop()
 
-        self.__sink(0)
+        self.__sink(1)
         return max_item
 
     def __swim(self, index):
@@ -30,11 +33,13 @@ class BinaryHeap:
         if not self.__return_max:
             a, b = b, a
 
-        while index > 0 and a < b:
+        while index > 1 and a < b:
             items[index], items[index // 2] = items[index // 2], items[index]
+
             index //= 2
             a = items[index // 2]
             b = items[index]
+
             if not self.__return_max:
                 a, b = b, a
 
@@ -68,11 +73,17 @@ class BinaryHeap:
 
 
 if __name__ == '__main__':
-    HEAP_ITEMS_QUANTITY = 5
-    bh = BinaryHeap()
-    for i in range(1, HEAP_ITEMS_QUANTITY + 1):
+    HEAP_ITEMS_QUANTITY = 10
+    bh = BinaryHeap(return_max=True)
+    for i in range(1, HEAP_ITEMS_QUANTITY):
         bh.insert(i)
 
     bh.insert(132)
-    for i in range(HEAP_ITEMS_QUANTITY + 1):
-        print("max item =", bh.del_element())
+    bh.insert(74)
+    bh.insert(282)
+    bh.insert(0)
+
+    for i in range(HEAP_ITEMS_QUANTITY + 4):
+        x = bh.del_element()
+        print(x)
+        # print()
