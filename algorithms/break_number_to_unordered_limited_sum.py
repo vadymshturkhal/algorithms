1e3151f08e2b2f9adf53413d6dc6ba0d914eb586
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-08-27 21:22:12 +0300

    Planning to implement number to limted sum of terms.

---
+++ algorithms/break_number_to_unordered_limited_sum.py
@@ -0,0 +1,10 @@
+def number_to_limited_sum_of_terms(number: int, terms: list):
+    pass
+
+
+if __name__ == '__main__':
+    NUMBER = 73
+    TERMS = [1, 2, 3, 5, 10, 15, 20, 50]
+
+    total_ways = number_to_limited_sum_of_terms(NUMBER, TERMS)
+    print(total_ways)
