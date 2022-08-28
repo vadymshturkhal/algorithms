18e69112df0fedd823cbfead5a00740cdda4ae51
Vadym Shturkhal <vadymshturkhal@gmail.com>
2022-08-28 13:38:02 +0300

    Implemented.

--- algorithms/break_number_to_unordered_limited_sum.py
+++ algorithms/break_number_to_unordered_limited_sum.py
@@ -1,5 +1,28 @@
-def number_to_limited_sum_of_terms(number: int, terms: list):
-    pass
+def number_to_limited_sum_of_terms(number: int, terms: list, is_filtered=False):
+    """
+    Theorem:
+        F(N: [n1, n2,...,nk]) = F(N - nk: [n1, n2,...,nk - 1]) + F(N: [n1, n2,...,nk - 1])
+        F(0: [n1, n2,...,nk]) = 1.
+        if N > sum([[n1, n2,...,nk]]): F(N: [n1, n2,...,nk]) = 0
+        F(-n: [n1, n2,...,nk]) = 0.
+    """
+    if not is_filtered:
+        terms = list(filter(lambda x: x < number, terms))
+
+    if number == 0:
+        return 1
+
+    if number > sum(terms):
+        return 0
+
+    if len(terms) <= 0:
+        return 0
+
+    F = number_to_limited_sum_of_terms
+    last_term = terms[-1]
+
+    res = F(number - last_term, terms[:-1], is_filtered=True) + F(number, terms[:-1], is_filtered=True)
+    return res
 
 
 if __name__ == '__main__':
@@ -7,4 +30,5 @@ if __name__ == '__main__':
     TERMS = [1, 2, 3, 5, 10, 15, 20, 50]
 
     total_ways = number_to_limited_sum_of_terms(NUMBER, TERMS)
-    print(total_ways)
+
+    print(total_ways)  # must be 4
